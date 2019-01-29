ac781e8940413057f93284f3230a2ceb5cbce479
Pippo Raimondi <filipporai@gmail.com>
2019-01-29 14:44:00 +0000

    adding more capaiblities

--- src/cross-platform-matrix/browserstack-client-matrix.js
+++ src/cross-platform-matrix/browserstack-client-matrix.js
@@ -1,11 +1,11 @@
 export const clients = {
   firefox_latest: {
-    browserName: 'firefox',
+    browserName: 'Firefox',
     os: 'Windows',
     os_version: '7',
   },
   chrome_latest: {
-    browserName: 'chrome',
+    browserName: 'Chrome',
     os: 'Windows',
     os_version: '10',
   },
@@ -15,6 +15,36 @@ export const clients = {
     os_version: '7',
     browser_version: '11.0',
     'browserstack.selenium_version': '3.8.0',
+  },
+  edge_latest: {
+    browserName: 'Edge',
+    os: 'Windows',
+    os_version: '10'
+  },
+  firefox_latest_macos: {
+    browserName: 'Firefox',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  chrome_latest_macos: {
+    browserName: 'Chrome',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  safari_latest_macos: {
+    browserName: 'Safari',
+    os: 'OS X',
+    os_version: 'Mojave',
+  },
+  iphone_8: {
+    device: 'iPhone 8',
+    os_version: '12.1',
+    real_mobile: 'true',
+  },
+  samsung_galaxy_8: {
+    device: 'Samsung Galaxy S8',
+    os_version: '7.0',
+    real_mobile: 'true',
   },
 }
 
